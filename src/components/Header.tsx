1d8339f7365e1cabd69f2cd65965548082566cbb
mjclaypool <mallory.claypool@gmail.com>
2024-08-27 14:20:22 -0400

    Finish mobile design
    Completed Hero section for mobile screen sizes.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import hamburgerIcon from "../../images/icon-hamburger.svg";
 
 export default function Header() {
   return (
-    <header className="flex justify-between p-6">
+    <header className="relative flex justify-between bg-n-white p-6 z-10">
       <img src={logo} alt="Easybank company logo" className="object-contain" />
       <img src={hamburgerIcon} alt="Hamburger menu icon" className="object-contain cursor-pointer" />
     </header>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,18 @@
+import heroImgD from "../../images/bg-intro-desktop.svg";
+import heroImgM from "../../images/bg-intro-mobile.svg";
+import mockups from "../../images/image-mockups.png"
+
 export default function Hero() {
   return (
-    <></>
+    <div className="relative">
+      <img src={mockups} alt="Hero image of Easybank mock-ups" className="h-[420px] absolute left-[50%] translate-x-[-50%] -top-[122px] object-cover z-0" />
+      <img src={heroImgM} alt="Background geometric shapes" className="lg:hidden h-[290px] w-full object-cover" />
+      <img src={heroImgD} alt="Background geometric shapes" className="hidden lg:block h-[290px] w-full object-cover" />
+      <div className="font-pubSans text-center px-6 pt-8 pb-[92px]">
+        <h1 className="text-p-dark-blue text-[40px] leading-tight">Next generation digital banking</h1>
+        <p className="text-n-grey-blue text-[15px] leading-relaxed pt-4 pb-8">Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.</p>
+        <button type="button" className="bg-gradient-to-r from-p-lime-green to-p-bright-cyan text-[14px] text-n-light-grey py-2.5 px-8 rounded-full">Request Invite</button>
+      </div>
+    </div>
   )
 }

7677f11eef1de0f51e60dfe33167500b278ba111
mjclaypool <mallory.claypool@gmail.com>
2024-08-27 13:57:05 -0400

    Complete Header and Footer components
    Completed the Header and Footer sections for mobile screen sizes.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,31 @@
+import logo from "../../images/logo-footer.svg";
+import facebook from "../../images/icon-facebook.svg";
+import youtube from "../../images/icon-youtube.svg";
+import twitter from "../../images/icon-twitter.svg";
+import pinterest from "../../images/icon-pinterest.svg";
+import instagram from "../../images/icon-instagram.svg";
+
 export default function Footer() {
   return (
-    <footer></footer>
+    <footer className="flex flex-col items-center gap-8 font-pubSans text-n-light-grey bg-p-dark-blue py-10">
+      <img src={logo} alt="Easybank company logo" className="object-contain" />
+      <div className="flex gap-4">
+        <img src={facebook} alt="Facebook logo" className="h-[24px] object-contain cursor-pointer" />
+        <img src={youtube} alt="Youtube logo" className="h-[24px] object-contain cursor-pointer" />
+        <img src={twitter} alt="Twitter logo" className="h-[24px] object-contain cursor-pointer" />
+        <img src={pinterest} alt="Pinterest logo" className="h-[24px] object-contain cursor-pointer" />
+        <img src={instagram} alt="Instagram logo" className="h-[24px] object-contain cursor-pointer" />
+      </div>
+      <div className="flex flex-col gap-3 text-[15px]">
+        <button type="button">About Us</button>
+        <button type="button">Contact</button>
+        <button type="button">Blog</button>
+        <button type="button">Careers</button>
+        <button type="button">Support</button>
+        <button type="button">Privacy Policy</button>
+      </div>
+      <button type="button" className="bg-gradient-to-r from-p-lime-green to-p-bright-cyan text-[14px] py-2.5 px-8 rounded-full">Request Invite</button>
+      <p className="text-[15px] text-n-grey-blue">Easybank. All Rights Reserved</p>
+    </footer>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
+import logo from "../../images/logo.svg";
+import hamburgerIcon from "../../images/icon-hamburger.svg";
+
 export default function Header() {
   return (
-    <header></header>
+    <header className="flex justify-between p-6">
+      <img src={logo} alt="Easybank company logo" className="object-contain" />
+      <img src={hamburgerIcon} alt="Hamburger menu icon" className="object-contain cursor-pointer" />
+    </header>
   )
 }
